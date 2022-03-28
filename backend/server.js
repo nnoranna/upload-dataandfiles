68a8aa43c4b07a6459d5e3e03a24ebdf39f42799
nnoranna <anagy.nora.anna@gmail.com>
2022-03-28 17:49:18 +0200

    Correct typos

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,60 @@
+const { json } = require("body-parser");
 const express = require("express");
-const fileupload = require("express-fileupload")
+const fileupload = require("express-fileupload");
+const { fstat } = require("fs");
 const path = require("path");
 const app = express(); // express is egy föggvény és ez lefut, és objektum jön vissza
 
-function getFunction(requiest, response){
-    response.sendFile(path.join(`${__dirname}/../frontend/index.html`));
+const dataLocation = path.join(`${__dirname}/../frontend/data/`);
+
+function getFunction(req, res){
+    res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
 }
 
 app.use(fileupload());
-
-
-
 app.get("/", getFunction);
 app.use("/upload", express.static(`${__dirname}/../frontend/upload`));
 app.use("/public", express.static(`${__dirname}/../frontend/public`));
+
+
+// If there is a data.json, read the data from the file, if not, use an empty Array
+/* let jsonData = [];
+try {
+    let data = fs.readFileSync(`${dataLocation}data.json`, error => {
+        if (error) {
+            console.log(error);
+        }
+    });
+    jsonData = JSON.parse(data);
+} catch (error) {
+    console.log(error);
+} */
+
+const uploads = path.join(`${__dirname}/../frontend/upload/`);
+
+app.post("/", (req, res) => {
+    // Upload image
+    const picture = req.files.picture;
+    const answer = {}
+    if (picture) {
+        console.dir(picture);
+        picture.mv(`${uploads}${picture.name}`);
+    }
+    answer.pictureName = picture.name
+    res.send(answer) // in Network in browser
+
+    // Upload data from form
+    /* const formData = req.body;
+    formData.image_name = picture.name;
+    jsonData.push(formData);
+
+    fs.writeFile(`${dataLocation}data.json`, JSON.stringify(jsonData), (error) => {
+        if (error) {
+            console.log(error);
+        }
+    }); */
+})
+
 
 const port = 9000;
 const ipAddress = `http://127.0.0.1:${port}`;
